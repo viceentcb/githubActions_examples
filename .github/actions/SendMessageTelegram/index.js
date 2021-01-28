47b88da9838e5b6bedac56d8e2c23090c016de2c
viceentcb <viceentcb@gmail.com>
2021-01-28 12:20:38 +0100

    Information

--- .github/actions/SendMessageTelegram/index.js
+++ .github/actions/SendMessageTelegram/index.js
@@ -1,6 +1,7 @@
 let core = require("@actions/core");
 let TelegramBot = require('node-telegram-bot-api');
 let github = require("@actions/github")
+github = github.context.payload;
 
 let token = core.getInput("TokenBot");
 let chatid=core.getInput("ChatId");
@@ -8,15 +9,15 @@ let name = core.getInput("Name");
 
 let bot = new TelegramBot(token, {polling: false});
 
- github = github.context.payload;
-
-
- console.log("----------------------")
- console.log(github)
-
 try {
-    bot.sendMessage(chatid, "Workflow ejecutado correctamente tras el último commit. Saludos "+name);
-  
+    bot.sendMessage(chatid, `Workflow ejecutado correctamente tras el último commit. Saludos ${name} \n 
+                             Autor: ${github.head_commit.author.name} \n
+                             Email autor:${github.head_commit.author.email} \n
+                             Commiter: ${github.head_commit.commiter.name} \n 
+                             Email Commiter: ${github.head_commit.commiter.email} \n
+                             Message commit : ${github.head_commit.message}
+                             Repository: ${github.head_commit.url} \n\n
+                             All informartion: ${github}`);
   } catch (error) {
     core.setFailed(error.message);
   }
