05ace771fed7eedcd744be8d39ea78a368ffd055
viceentcb <viceentcb@gmail.com>
2021-01-28 11:30:18 +0100

    proof nosecuanto

--- .github/actions/SendMessageTelegram/index.js
+++ .github/actions/SendMessageTelegram/index.js
@@ -7,11 +7,25 @@ const name = core.getInput("Name");
 
 const bot = new TelegramBot(token, {polling: false});
 
+
+
 try {
     bot.sendMessage(chatid, "Workflow ejecutado correctamente tras el último commit. Saludos "+name);
+
+
+    bot.on('polling_error', (error) => {
+      console.log("----------------");  // => 'EFATAL'
+
+      console.log(error.code);  // => 'EFATAL'
+    });
   
   } catch (error) {
     core.setFailed(error.message);
+
+    bot.on('polling_error', (error) => {
+      console.log("----------------");  // => 'EFATAL'
+      console.log(error.code);  // => 'EFATAL'
+    });
   }
 
 core.setOutput("message", "Mensaje enviado con exito");
