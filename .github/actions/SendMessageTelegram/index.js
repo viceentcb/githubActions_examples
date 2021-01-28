e308c791d79f004a74b8f78ff3e59b3d883a7f1a
viceentcb <viceentcb@gmail.com>
2021-01-28 11:34:49 +0100

    profporfprof

--- .github/actions/SendMessageTelegram/index.js
+++ .github/actions/SendMessageTelegram/index.js
@@ -1,31 +1,19 @@
-const core = require("@actions/core");
-const TelegramBot = require('node-telegram-bot-api');
+let core = require("@actions/core");
+let TelegramBot = require('node-telegram-bot-api');
 
-const token = core.getInput("TokenBot");
-const chatid=core.getInput("ChatId");
-const name = core.getInput("Name");
+let token = core.getInput("TokenBot");
+let chatid=core.getInput("ChatId");
+let name = core.getInput("Name");
 
-const bot = new TelegramBot(token, {polling: false});
+let bot = new TelegramBot(token, {polling: false});
 
 
 
 try {
     bot.sendMessage(chatid, "Workflow ejecutado correctamente tras el último commit. Saludos "+name);
-
-
-    bot.on('polling_error', (error) => {
-      console.log("----------------");  // => 'EFATAL'
-
-      console.log(error.code);  // => 'EFATAL'
-    });
   
   } catch (error) {
     core.setFailed(error.message);
-
-    bot.on('polling_error', (error) => {
-      console.log("----------------");  // => 'EFATAL'
-      console.log(error.code);  // => 'EFATAL'
-    });
   }
 
 core.setOutput("message", "Mensaje enviado con exito");
